3a7c502f8422df6f4906f9c477e7e3740ff16374
vcky4 <okon.victor.u@gmail.com>
2023-11-09 01:31:15 +0100

    feet: add header to home

--- frontend/protraveller/src/pages/Home.js
+++ frontend/protraveller/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Post from '../components/Post';
 import FilterBar from '../components/FilterBar';
-import home from '../images/home.jpg'
+import { Header } from '../components/Header';
 
 
 const Home = () => {
@@ -18,30 +18,33 @@ const Home = () => {
   const filteredPosts = posts.filter((post) => locationFilter === 'All' || post.location === locationFilter);
 
   return (
-    <div
-      className="flex justify-center h-screen bg-cream"
-    // style={{
-    //   backgroundImage: `url(${home})`,
-    //   backgroundSize: 'cover', // Adjust to your needs
-    //   backgroundPosition: 'center', // Adjust to your needs
-    // }}
-    >
-      {/* Dark overlay */}
-      <div
-        className="absolute inset-0 bg-black opacity-50"
-        style={{ mixBlendMode: 'multiply' }}
-      />
+    <>
 
-      <div className="container mx-auto p-4 z-20">
-        <h1 className="text-3xl font-bold mb-4">Welcome to ProTraveller</h1>
-        <FilterBar locationFilter={locationFilter} setLocationFilter={setLocationFilter} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {filteredPosts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
+      <div
+        className="flex justify-center h-screen bg-cream"
+      // style={{
+      //   backgroundImage: `url(${home})`,
+      //   backgroundSize: 'cover', // Adjust to your needs
+      //   backgroundPosition: 'center', // Adjust to your needs
+      // }}
+      >
+        {/* Dark overlay */}
+        <div
+          className="absolute inset-0 bg-black opacity-50"
+          style={{ mixBlendMode: 'multiply' }}
+        />
+        <div className="container mx-auto p-4 z-20">
+          <Header />
+          <h1 className="text-3xl font-bold mb-4">Explore The Beauty</h1>
+          <FilterBar locationFilter={locationFilter} setLocationFilter={setLocationFilter} />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {filteredPosts.map((post) => (
+              <Post key={post.id} post={post} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
