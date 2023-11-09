68af5b7835d0ecd6f19eea68bb3a6a29532cd938
vcky4 <okon.victor.u@gmail.com>
2023-11-09 23:31:49 +0100

    complete feature section

--- frontend/protraveller/src/components/Features.js
+++ frontend/protraveller/src/components/Features.js
@@ -1,5 +1,8 @@
 import React from "react"
 import ext from "../images/ept.png"
+import confident from "../images/confident.png"
+import adventure from "../images/adventure.png"
+import respect from "../images/respect.png"
 
 export const Features = () => {
     return (
@@ -11,17 +14,17 @@ export const Features = () => {
                     <p className="text-center text-white playfair">​Uncover hidden gems<br /> with insights from local<br /> guides.</p>
                 </div>
                 <div className="flex flex-col items-center gap-4 px-8 py-8 background border border-color border-4 rounded-2xl">
-                    <img src={ext} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
+                    <img src={confident} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
                     <h2 className="text-2xl font-semibold text-center text-white playfair">​​Explore<br /> Confidently</h2>
                     <p className="text-center text-white playfair">​​Travel with confidence<br />  using tips from<br /> experienced adventurers.</p>
                 </div>
                 <div className="flex flex-col items-center gap-4 px-8 py-8 background border border-color border-4 rounded-2xl">
-                    <img src={ext} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
+                    <img src={adventure} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
                     <h2 className="text-2xl font-semibold text-center text-white playfair">​​Affordable <br /> Adventures</h2>
                     <p className="text-center text-white playfair"> ​Travel smart and find the<br /> best deals</p>
                 </div>
                 <div className="flex flex-col items-center gap-4 px-8 py-8 background border border-color border-4 rounded-2xl">
-                    <img src={ext} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
+                    <img src={respect} alt="" className="w-18 h-16" style={{ filter: 'brightness(0) invert(1)' }} />
                     <h2 className="text-2xl font-semibold text-center text-white playfair">​Respect & <br /> Connect</h2>
                     <p className="text-center text-white playfair">​​Learn to engage<br />respectfully with different<br /> cultures.</p>
                 </div>
