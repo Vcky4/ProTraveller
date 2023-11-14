9cf4eeb529e82e650d29e394e5c5227290043228
vcky4 <okon.victor.u@gmail.com>
2023-11-14 20:42:27 +0100

    tries

--- frontend/protraveller/src/pages/SignUp.js
+++ frontend/protraveller/src/pages/SignUp.js
@@ -2,15 +2,43 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import flight2 from '../images/flight2.jpg';
 import logo from '../images/logo.png'
-
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password: '',
+    })
 
+    //sign up
     const handleSignUp = () => {
         // Handle sign-up logic
+        if(signUpData.password !== confirmPassword) {
+            alert('Passwords do not match');
+            // toast.error('Passwords do not match', {
+            //     position: 'top-right',
+            //     autoClose: 3000, // Close the alert after 3 seconds
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     pauseOnHover: true,
+            //     draggable: true,
+            //     progress: undefined,
+            //   });
+        }
+        fetch('http://127.0.0.1:8000/api/signup/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(signUpData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert(data.detail)
+                console.log(data)
+            })
+            .catch(err => console.log(err))
     };
 
     return (
@@ -33,16 +61,22 @@ const SignUp = () => {
                 <h2 className="text-2xl choc-brown font-semibold mb-4">Sign Up</h2>
                 <input
                     type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="UserName"
+                    value={signUpData.username}
+                    onChange={(e) => setSignUpData({
+                        ...signUpData,
+                        username: e.target.value
+                    })}
                     className="w-full border choc-brown rounded py-2 px-3 mb-2"
                 />
                 <input
                     type="password"
                     placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={signUpData.password}
+                    onChange={(e) => setSignUpData({
+                        ...signUpData,
+                        password: e.target.value
+                    })}
                     className="w-full border choc-brown rounded py-2 px-3 mb-2"
                 />
                 <input
